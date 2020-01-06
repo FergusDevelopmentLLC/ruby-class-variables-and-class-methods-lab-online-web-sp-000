5310789bacc792b23636a7052fb952f14fa2876e
Will Carter <will.carter@fergusllc.com>
2020-01-06 21:49:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,9 @@ class Song
   attr_accessor :name, :artist, :genre
 
   def initialize(song_name, song_artist, song_genre)
-    name = name
-    artist = artist
-    genre = genre
+    name = song_name
+    artist = song_artist
+    genre = song_genre
   end
 
 end
