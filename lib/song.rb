d068cecedac7e7cd4c14556b8c53ca1e94d8956b
Will Carter <will.carter@fergusllc.com>
2020-01-06 22:02:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
   @@count = 0
   @@artists = []
   @@genres = []
-  
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
@@ -13,7 +13,7 @@ class Song
     if @@artists.include? artist == false
       @@artists << artist
     end
-    
+
   end
 
   def self.count
